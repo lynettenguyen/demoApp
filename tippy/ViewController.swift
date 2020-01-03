168ca74b1a5aaf814e75cbea44b61b114cdda2fc
Lynette Nguyen <lynettenguyen0909@gmail.com>
2020-01-03 15:28:58 -0800

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Lynette Nguyen on 1/3/20.
+//  Copyright © 2020 Lynette Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
